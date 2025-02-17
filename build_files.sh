dff1c5c4a0718acbef675010742289c9af5deec7
jer-hub <jerwin.carreon@jmc.edu.ph>
2025-02-17 23:24:07 +0800

    python version

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput
-python3.11 manage.py migrate
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
